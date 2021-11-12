487501d793dbc72160f1b821c6d3e6ab35bef6ee
Administrator <Administrator@DESKTOP-I5OJSC7>
2021-11-12 11:47:16 +0900

    초기 커밋.

---
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await Task.Run(LoadEmployees);
+        }
+
+        private void LoadEmployees()
+        {
+            System.Threading.Thread.Sleep(3000);
+            Employee e1 = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "John",
+                LastName = "Hastings",
+                Email = "David@pragimtech.com",
+                DateOfBrith = new DateTime(1980, 10, 5),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentId = 1, DepartmentName = "Shin" },
+                PhotoPath = "images/john.png"
+            };
+
+            Employees = new List<Employee> { e1 };
+        }
+    }
+}
---
+++ Hello/MainApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello
+{
+    class MainApp
+    {
+        // 프로그램 실행이 시작되는 곳
+        static void Main(string[] args)
+        {
+            for (int i = 2; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                Console.WriteLine(i + "*" + j + "=" + i*j );
+            }
+            WriteLine("Hello!"); // Hello, World를 프롬프트에 출력
+        }
+    }
+}
